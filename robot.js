d1ab0116f658df741a9de75d9ebc05548aa030f6
RONTheCookie <ronthecookie0101@gmail.com>
2017-10-04 14:10:49 +0300

    oh they changed it

--- robot.js
+++ robot.js
@@ -14,9 +14,9 @@ client.login(require("./token"));
 
 client.on('ready', () => {
     console.log(`Warvale bot is now ready to serve ${client.users.size} Warvale players.\nThis bot was made by Warvale and is not to be self-hosted.`);
-    client.user.setGame(`with ${client.users.size} warvalers!`);
+    client.user.setActivity(`with ${client.users.size} warvalers!`);
     client.setInterval(()=>{
-        client.user.setGame(`with ${client.users.size} warvalers!`);
+        client.user.setActivity(`with ${client.users.size} warvalers!`);
     }, 1000*60*5);
 });
 
