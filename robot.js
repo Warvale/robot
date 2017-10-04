9bcdddb4e2f5493c9d6a38ef6efa3909e854ecf4
jellz <jellzbo@gmail.com>
2017-10-04 21:22:00 +1100

    Begin rewrite + add ping command.

---
+++ robot.js
@@ -0,0 +1,32 @@
+// DEPENDENCIES
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const PersistentCollection = require("djs-collection-persistent");
+const timestamp = require('console-timestamp');
+const animals = require('relevant-animals');
+// BOT STUFF, NOT DEPENDENCIES
+const config = require('./config.json');
+const prefix = config.prefix;
+const embed = new Discord.MessageEmbed();
+// USE TOKEN FROM FILE TO LOGIN
+client.login(require("./token"));
+
+
+client.on('ready', () => {
+    console.log(`Warvale bot is now ready to serve ${client.users.size} Warvale players.\nThis bot was made by Warvale and is not to be self-hosted.`);
+});
+
+
+client.on('message', (msg) => {
+    var args = msg.content.split(' ').slice(1);
+    var result = args.join(' ');
+
+    if (msg.author.bot) return;
+
+    if (msg.content.startsWith(prefix + 'ping')) {
+        embed.setColor("0xf56d05");
+        embed.setDescription(`Pong! Use \`${config.prefix}help\` for commands.`);
+        msg.channel.send({ embed: embed });
+    }
+    
+});
