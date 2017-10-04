43065b7a9e4c6d960ad3ae0910045145fb44b068
jellz <jellzbo@gmail.com>
2017-10-04 22:17:31 +1100

    Add !about command.

--- robot.js
+++ robot.js
@@ -1,32 +1,42 @@
 // DEPENDENCIES
-const Discord = require('discord.js');
+const Discord = require(`discord.js`);
 const client = new Discord.Client();
-const PersistentCollection = require("djs-collection-persistent");
-const timestamp = require('console-timestamp');
-const animals = require('relevant-animals');
+const PersistentCollection = require(`djs-collection-persistent`);
+const timestamp = require(`console-timestamp`);
+const animals = require(`relevant-animals`);
 // BOT STUFF, NOT DEPENDENCIES
-const config = require('./config.json');
+const config = require(`./config.json`);
 const prefix = config.prefix;
 const embed = new Discord.MessageEmbed();
 // USE TOKEN FROM FILE TO LOGIN
-client.login(require("./token"));
+client.login(require(`./token`));
 
 
-client.on('ready', () => {
+client.on(`ready`, () => {
     console.log(`Warvale bot is now ready to serve ${client.users.size} Warvale players.\nThis bot was made by Warvale and is not to be self-hosted.`);
 });
 
 
-client.on('message', (msg) => {
-    var args = msg.content.split(' ').slice(1);
-    var result = args.join(' ');
+client.on(`message`, (msg) => {
+    var args = msg.content.split(` `).slice(1);
+    var result = args.join(` `);
 
     if (msg.author.bot) return;
 
-    if (msg.content.startsWith(prefix + 'ping')) {
-        embed.setColor("0xf56d05");
+    if (msg.content.startsWith(prefix + `ping`)) {
+        embed.setColor(`0xf56d05`);
         embed.setDescription(`Pong! Use \`${config.prefix}help\` for commands.`);
         msg.channel.send({ embed: embed });
+    } else 
+
+    if (msg.content.startsWith(prefix + `about`)) {
+        embed.setColor(`0xf56d05`);
+        embed.setDescription(`Warvale is an upcoming Minecraft network featuring PVP games.`);
+        embed.addField(`Players`, msg.guild.members.size, true);
+        embed.addField(`Links`, `[GitHub](https://github.com/Warvale/robot)\n[Warvale](https://warvale.net)`, true);
+        msg.channel.send({ embed: embed });
     }
+
+
     
 });
