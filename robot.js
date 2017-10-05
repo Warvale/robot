32a714c2642e74133b338d7e47bc9150269b3cad
jellz <jellzbo@gmail.com>
2017-10-05 18:25:59 +1100

    :bird: Add Twitter link to !about.

--- robot.js
+++ robot.js
@@ -42,7 +42,7 @@ client.on(`message`, (msg) => {
         embed.setColor(`0xf56d05`);
         embed.setDescription(`Warvale is an upcoming Minecraft network featuring PVP games.`);
         embed.addField(`Players`, msg.guild.memberCount, true);
-        embed.addField(`Links`, `[GitHub](https://github.com/Warvale/robot)\n[Warvale](https://warvale.net)`, true);
+        embed.addField(`Links`, `[GitHub](https://github.com/Warvale/robot)\n[Warvale](https://warvale.net)\n[Twitter](https://twitter.com/warvalenetwork)`, true);
         msg.channel.send({ embed: embed });
     } else
 
