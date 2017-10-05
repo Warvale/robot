842ed1360db0c77ff0b6b1aef5d1a3b86fc99c44
jellz <jellzbo@gmail.com>
2017-10-05 20:25:47 +1100

    Add log function fallback.

--- log/LogManager.js
+++ log/LogManager.js
@@ -1,5 +1,6 @@
 var client = require(`../robot`).client;
 var log = function(content) {
+    if (!client.guilds.first().channels.find(`name`, `warvale-bot-logs`)) return;
     var now = new Date();
     var logChannel = client.guilds.first().channels.find(`name`, `warvale-bot-logs`);
     logChannel.send(`\`[${'hh:mm:ss'.timestamp}]\`${content}`);
