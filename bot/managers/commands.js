213fb6b47927045115077dd4bfd31e6f14f9cc5c
Eric <ehock7229@outlook.com>
2017-10-06 18:32:55 -0400

    Add ban command

--- bot/managers/commands.js
+++ bot/managers/commands.js
@@ -3,7 +3,10 @@ module.exports = {
     "help": require('../commands/help'),
     "about": require('../commands/about'),
     "ping": require('../commands/ping'),
-    "eval": require('../commands/eval')
+    "eval": require('../commands/eval'),
+
+    //punish commands
+    "ban": require('../commands/punish/ban')
 
 };
 
