cbd7ac531035a3113b9ece79e3df41fd5f1e3082
CommanderG2 <commanderg102@gmail.com>
2017-10-06 11:24:05 -0400

    Redoing ban

--- robot.js
+++ robot.js
@@ -84,25 +84,6 @@ client.on(`message`, (msg) => {
         }
     }
 });
-
-    if (msg.content,toLowerCase().startsWith(prefix + `ban`)) {
-        exports.run = (client, message, args) => {
-            let reason = args.slice(1).join(' ');
-            let user = message.mentions.users.first();
-            let modlog = client.channels.find('name', 'warvale-bot-logs');
-            if (reason.length < 1) return message.reply('Please input a reason.');
-            if (message.mentions.users.size < 1) return message.reply('Please mention someone to ban them.').catch(console.error); 
-            
-            if (!message.guild.member(user).bannable) return message.reply('I do not have permission to ban that member.');
-            message.guild.ban(user, 2);
-
-            const embed = new Discord.RichEmbed()
-            .setColor(0xff0000)
-            .setTimestamp()
-            .setDescription(`**Action:** Ban\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
-          return client.channels.get(modlog.id).send({embed});
-        };
-    }
     
 
 // LOGGING HANDLING & EVENTS
