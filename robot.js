f427218dd4394d0541714f92f52222f148929c7a
jellz <24366082+jellz@users.noreply.github.com>
2017-10-08 15:49:45 +1100

    Fix false-positives on the message edit logging.

--- robot.js
+++ robot.js
@@ -112,6 +112,7 @@ client.on(`messageDelete`, (msg) => {
 
 client.on(`messageUpdate`, (oldMessage, newMessage) => {
     if (oldMessage.author.bot) return;
+    if (oldMessage.content === newMessage.content) return;
     log(`A message by **${oldMessage.author.tag} (${oldMessage.author.id})** was edited.\n\n**Old:** ${oldMessage.content}\n**New:** ${newMessage.content}`);
 });
 
